f4a0ad17cebbeb454610e66535a1cd01b0fbb10e
famdrum <ko.oles999@gmail.com>
2017-02-01 13:06:37 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Project1',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Oles',
+    author_email='',
+    description=''
+)
